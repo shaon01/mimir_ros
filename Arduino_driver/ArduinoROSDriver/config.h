dd4de3c857565fd57e194c03c37a9d9e481f0914
shaon01 <shaoun46@gmail.com>
2020-09-20 20:10:34 +0200

    motor forward works

---
+++ Arduino_driver/ArduinoROSDriver/config.h
@@ -0,0 +1,36 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+//电机引脚
+#define PWMD 12    //D电机转速
+#define DIRD1 7 
+#define DIRD2 8  //D电机方向
+
+#define PWMC 10    //C电机转速
+#define DIRC1 28 
+#define DIRC2 26  //C电机方向
+
+#define PWMB 11   //B电机转速
+#define DIRB1 5 
+#define DIRB2 6  //B电机方向
+
+#define PWMA 9    //A电机转速
+#define DIRA1 24    //26  
+#define DIRA2 22    //27  //A电机方向
+
+//in our configuration
+//   (A)//   x+     \\(B)
+//   
+//   y+
+//
+//   (C)\\          //(D)
+
+float xn=0.08725, yn=0.105, r=0.03, K=abs(xn)+abs(yn); //m
+int max_rmp=330;
+float resolution=2*PI*r/1440.0;
+
+#define constrain(amt,low,high) \
+  ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+
+
+#endif
